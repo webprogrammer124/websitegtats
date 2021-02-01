ba4abee583abac4c3a193b4faa2889801f071cd3
Your Name <you@example.com>
2021-02-01 12:10:59 +0500

    done

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Student-PC\\Desktop\\Gatsby\\marvelous-mango-3b252\\.cache\\dev-404-page.js"))),
+  "component---src-templates-advanced-js": hot(preferDefault(require("C:\\Users\\Student-PC\\Desktop\\Gatsby\\marvelous-mango-3b252\\src\\templates\\advanced.js"))),
+  "component---src-templates-blog-js": hot(preferDefault(require("C:\\Users\\Student-PC\\Desktop\\Gatsby\\marvelous-mango-3b252\\src\\templates\\blog.js"))),
+  "component---src-templates-page-js": hot(preferDefault(require("C:\\Users\\Student-PC\\Desktop\\Gatsby\\marvelous-mango-3b252\\src\\templates\\page.js"))),
+  "component---src-templates-portfolio-js": hot(preferDefault(require("C:\\Users\\Student-PC\\Desktop\\Gatsby\\marvelous-mango-3b252\\src\\templates\\portfolio.js"))),
+  "component---src-templates-post-js": hot(preferDefault(require("C:\\Users\\Student-PC\\Desktop\\Gatsby\\marvelous-mango-3b252\\src\\templates\\post.js"))),
+  "component---src-templates-project-js": hot(preferDefault(require("C:\\Users\\Student-PC\\Desktop\\Gatsby\\marvelous-mango-3b252\\src\\templates\\project.js")))
+}
+
